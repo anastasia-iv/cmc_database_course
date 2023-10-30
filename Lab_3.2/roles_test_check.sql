89c0770a723055f8960758a7b27c8c1ea3581fe3
anastasia-iv <100083479+anastasia-iv@users.noreply.github.com>
2023-10-30 04:40:10 +0300

    Update roles_test_check.sql

--- Lab_3.2/roles_test_check.sql
+++ Lab_3.2/roles_test_check.sql
@@ -3,15 +3,16 @@ SET ROLE test;
 
 SELECT * FROM stat.tickets LIMIT 5;
 UPDATE stat.tickets SET price_dollars = 5 WHERE ticket_id = 1;
-INSERT INTO stat.tickets (student_id,price_dollars, info, location_id, date_of_purchase) 
-SELECT 1, 5, jsonb_build_object('type', "Входной", 'privileges', "Обычный" ), 1, timestamp '2023-10-29 8:00:00';
+INSERT INTO stat.tickets (student_id,price_dollars, info, location_id, date_of_purchase)
+SELECT 1, 5, jsonb_build_object('type', 'Входной', 'privileges', 'Обычный'), 1, timestamp '2023-10-29 8:00:00';
 
-UPDATE stat.students SET course = 3 WHERE course = 2
+UPDATE stat.students SET course = 3 WHERE course = 2;
 
 SELECT * FROM stat.location LIMIT 5;
 
-UPDATE location_list SET location_name = "Пушкинский музей" WHERE location_id = 1
+UPDATE location_list SET location_name = 'Пушкинский музей' WHERE location_id = 1;
 SELECT * FROM location_list LIMIT 5;
+!!!!ERROR:  permission denied for view location_list -> нет разрешений на изменение 
 
 --testing role role_new
 
